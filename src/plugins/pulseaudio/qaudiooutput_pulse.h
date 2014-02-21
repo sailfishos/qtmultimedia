50dbdc87c09504556666321bd22f3d3226e9567a
Juho Hämäläinen <juho.hamalainen@tieto.com>
2014-02-21 10:48:18 +0200

    Resource policy for QAudioOutput.
    Change-Id: I561b8b6216d94f56362f52d65482f6e9cd6d7f3a
    
    Conflicts:
    	src/plugins/pulseaudio/qaudiooutput_pulse.cpp

--- src/plugins/pulseaudio/qaudiooutput_pulse.h
+++ src/plugins/pulseaudio/qaudiooutput_pulse.h
@@ -58,6 +58,8 @@
 
 #include <pulse/pulseaudio.h>
 
+class QMediaPlayerResourceSetInterface;
+
 QT_BEGIN_NAMESPACE
 
 class QPulseAudioOutput : public QAbstractAudioOutput
@@ -104,10 +106,19 @@ private:
     bool open();
     void close();
     qint64 write(const char *data, qint64 len);
+    void internalSuspend();
+
+    void restartReleaseTimer();
+    void stopReleaseTimer();
 
 private Q_SLOTS:
     void userFeed();
     void onPulseContextFailed();
+    void handleResourcesGranted();
+    void handleResourcesLost();
+    void handleResourcesDenied();
+
+    void handleRelease();
 
 private:
     QByteArray m_device;
@@ -115,6 +126,7 @@ private:
     QAudioFormat m_format;
     QAudio::Error m_errorState;
     QAudio::State m_deviceState;
+    QAudio::State m_wantedState;
     bool m_pullMode;
     bool m_opened;
     QIODevice *m_audioSource;
@@ -136,6 +148,8 @@ private:
 
     qreal m_volume;
     pa_sample_spec m_spec;
+    QMediaPlayerResourceSetInterface *m_resources;
+    QTimer *m_releaseTimer;
 };
 
 class PulseOutputPrivate : public QIODevice

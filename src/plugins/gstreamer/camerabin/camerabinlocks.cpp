667b3a7161aee8f2ad18a4f00e7203a5f80147a2
Mohammed Hassan <mohammed.hassan@jolla.com>
2015-06-03 17:44:23 +0300

    [qtmultimedia] MER: Disable exposure and white balance locks for now. Fixes JB#29490
    They are unsupported and cause the search and lock status to always be set to unlocked
    
    We disable it now until we implement it later in droidcamsrc

--- src/plugins/gstreamer/camerabin/camerabinlocks.cpp
+++ src/plugins/gstreamer/camerabin/camerabinlocks.cpp
@@ -64,10 +64,10 @@ QCamera::LockTypes CameraBinLocks::supportedLocks() const
 #if GST_CHECK_VERSION(1, 2, 0)
     if (GstPhotography *photography = m_session->photography()) {
         if (gst_photography_get_capabilities(photography) & GST_PHOTOGRAPHY_CAPS_WB_MODE)
-            locks |= QCamera::LockWhiteBalance;
+/*            locks |= QCamera::LockWhiteBalance; */
         if (g_object_class_find_property(
                     G_OBJECT_GET_CLASS(m_session->cameraSource()), "exposure-mode")) {
-            locks |= QCamera::LockExposure;
+/*            locks |= QCamera::LockExposure; */
         }
     }
 #endif
